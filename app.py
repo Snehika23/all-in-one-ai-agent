ff2e75f05a9dea7211f595923146321a569a1948
Snehika Amudalapalli <101037055+Snehika23@users.noreply.github.com>
2026-09-15 00:03:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools import DuckDuckGoSearchRun
-from langchain_community.agent_toolkits import create_pandas_dataframe_agent
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain.prompts import PromptTemplate
 import pandas as pd
 import os
